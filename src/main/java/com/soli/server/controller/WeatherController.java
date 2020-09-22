a43c3bbfa1abfbc6c3b8c48db82a6f7a0e87805d
wangzhengwen <wangzw9844@163.com>
2020-09-22 16:02:38 +0800

    更换天气接口app密钥

--- src/main/java/com/soli/server/controller/WeatherController.java
+++ src/main/java/com/soli/server/controller/WeatherController.java
@@ -164,7 +164,7 @@ public class WeatherController extends LambkitController {
 
         String cname = pg.getStr("cname");
 
-        String url_ptah = "https://tianqiapi.com/api?version=v1&appid=66734781&appsecret=5b80caba77ff9738d5bac0b884339e1a&city="+cname+"";
+        String url_ptah = "https://tianqiapi.com/api?version=v1&appid=66734781&appsecret=xX1MZFk2&city="+cname+"";
 
         try {
             OkHttpClient client = new OkHttpClient().newBuilder()
