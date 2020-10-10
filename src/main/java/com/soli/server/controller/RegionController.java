0c4e46059626b9b4221586e710dfeb4d1fca47fe
yangxueyang <1612219848@qq.com>
2020-10-10 09:12:34 +0800

    提交最新经济作物

---
+++ src/main/java/com/soli/server/controller/RegionController.java
@@ -0,0 +1,137 @@
+package com.soli.server.controller;
+
+
+import com.jfinal.aop.Clear;
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.lambkit.common.util.StringUtils;
+import com.lambkit.web.controller.LambkitController;
+import com.soli.server.utils.Co;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Clear
+public class RegionController extends LambkitController {
+
+    /**
+     * @return void
+     * @Author queer
+     * @Description //TODO 获取省份列表
+     * @Date 15:44 2019/12/20
+     * @Param []
+     **/
+    public void getProvince() {
+        List<Record> list = Db.find("select pname from ld_ch_province");
+        renderJson(Co.ok("data", Ret.ok("data",list)));
+    }
+
+    /**
+     * @return void
+     * @Author queer
+     * @Description //TODO 根据省份名称获取市列表
+     * @Date 15:45 2019/12/20
+     * @Param []
+     **/
+    public void getCity() {
+        String pname = getPara("pname");
+        if (StringUtils.isBlank(pname)) {
+            renderJson(Co.fail("msg", "请选择省份"));
+        }
+        List<Record> list = Db.find("select cname from ld_ch_city where pname = ? order by pyname ", pname);
+        renderJson(Co.ok("data", Ret.ok("data",list)));
+    }
+
+    /**
+     * @return void
+     * @Author queer
+     * @Description //TODO 根据省份名称和市名称获取县列表
+     * @Date 15:49 2019/12/20
+     * @Param []
+     **/
+    public void getCounty() {
+        String pname = getPara("pname");
+        String cname = getPara("cname");
+        if (StringUtils.isBlank(pname)) {
+            renderJson(Co.fail("msg", "请选择省份"));
+        }
+        if (StringUtils.isBlank(cname)) {
+            renderJson(Co.fail("msg", "请选择市区"));
+        }
+        List<Record> list = Db.find("select name from ld_ch_county where pname = ? and cname = ? order by pyname ", pname, cname);
+        renderJson(Co.ok("data", list));
+    }
+
+
+    /**
+     * @return void
+     * @Author queer
+     * @Description //TODO 模糊查询省市县列表
+     * @Date 15:49 2019/12/20
+     * @Param []
+     **/
+    public void getCountyByName() {
+
+        String name = getPara("name");
+
+        if (StringUtils.isBlank(name)) {
+            renderJson(Co.ok("data", Ret.ok("errorMsg", "请输入名称")));
+        }
+
+        List<Record> provinceList = Db.find("select gid, pname as name  , 0 as type from ld_ch_province where pname like '%" + name + "%' ");
+        List<Record> cityList = Db.find("select gid, cname as name  , 1 as type from ld_ch_city where cname like '%" + name + "%'");
+        List<Record> countyList = Db.find("select gid, name , 2 as type from ld_ch_county where name like '%" + name + "%'");
+
+        List<Record> recordList = new ArrayList<>();
+        recordList.addAll(provinceList);
+        recordList.addAll(cityList);
+        recordList.addAll(countyList);
+
+        List<Record> result=new ArrayList<>();
+
+        for (Record record : recordList) {
+            if (!record.getStr("name").equals("市辖区")) {
+                result.add(record);
+            }
+        }
+
+        renderJson(Co.ok("data",Ret.ok("data",result)));
+    }
+
+
+
+    /**
+     * @return void
+     * @Author queer
+     * @Description //TODO 模糊查询省市县最大最小坐标
+     * @Date 15:49 2019/12/20
+     * @Param []
+     **/
+    public void getgemoById() {
+
+        Integer id=getParaToInt("id");
+        Integer type=getParaToInt("type");
+
+        if (id==null) {
+            renderJson(Co.ok("data", Ret.fail("errorMsg", "请输入id")));
+        }
+        if (type==null) {
+            renderJson(Co.ok("data", Ret.fail("errorMsg", "请输入类型")));
+        }
+        Record list =null;
+        if(type==0){
+             list = Db.findFirst("SELECT st_xmin(geom),st_ymin(geom),st_xmax(geom),st_ymax(geom) FROM ld_ch_province  where \"gid\"=?", id);
+        }
+        if(type==1){
+            list = Db.findFirst("SELECT st_xmin(geom),st_ymin(geom),st_xmax(geom),st_ymax(geom) FROM ld_ch_city  where \"gid\"=?", id);
+        }
+        if(type==2){
+            list = Db.findFirst("SELECT st_xmin(geom),st_ymin(geom),st_xmax(geom),st_ymax(geom) FROM ld_ch_county  where \"gid\"=?", id);
+        }
+
+        renderJson(Co.ok("data",Ret.ok("data",list)));
+    }
+
+
+}
