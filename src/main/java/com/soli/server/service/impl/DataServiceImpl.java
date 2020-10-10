759d8fda7ec5879ac846fc27ea8ebb5c5fee91e1
yangxueyang <1612219848@qq.com>
2020-10-10 15:19:12 +0800

    提交

--- src/main/java/com/soli/server/service/impl/DataServiceImpl.java
+++ src/main/java/com/soli/server/service/impl/DataServiceImpl.java
@@ -251,7 +251,7 @@ public class DataServiceImpl extends LambkitModelServiceImpl<Data> implements Da
         Record areaCount = Db.findFirst("SELECT sum(st_area(ST_Transform(geom,4527)))*0.0015 as count from tr_tiankuai");
         List<Record> typesArea = Db.find("SELECT type,sum(st_area(ST_Transform(geom,4527)))*0.0015 as area from tr_tiankuai GROUP BY type");
         return Ret.ok("typesNum", typesNum).set("typesArea", typesArea)
-                .set("numCount", numCount.getDouble("count")*0.0015)
+                .set("numCount", numCount.getDouble("count"))
                 .set("areaCount", areaCount.getDouble("count")*0.0015);
     }
 
