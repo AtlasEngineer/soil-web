e27d5088d9b3651c71c314715b1d171bbb6935fe
XuZhonghao0808 <xuzhonghao0808@163.com>
2020-10-13 17:03:49 +0800

    添加读取tif的dbf生成sld

--- src/main/java/com/soli/server/utils/IssueTiffUtils.java
+++ src/main/java/com/soli/server/utils/IssueTiffUtils.java
@@ -4,6 +4,8 @@ import com.jfinal.kit.Kv;
 import com.jfinal.kit.PathKit;
 import com.jfinal.plugin.activerecord.Db;
 import com.lambkit.Lambkit;
+import com.linuxense.javadbf.DBFField;
+import com.linuxense.javadbf.DBFReader;
 import com.soli.lambkit.start.GeoServerConfig;
 import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
@@ -20,11 +22,11 @@ import org.geotools.gce.geotiff.GeoTiffReader;
 
 import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class IssueTiffUtils {
@@ -93,13 +95,21 @@ public class IssueTiffUtils {
         System.out.println("s" + s);
         File files = new File(s);
         String tifPath = null;
+        boolean existDbf = false;
+        String tifDbfPath = null;
         String[] fileList = files.list();
         List<String> list = new ArrayList<>();
         for (String y : fileList) {
             File file1 = new File(s + "/" + y);
             System.out.println("file1:" + file1.getName());
             if (file1.isFile()) {
-                String s1 = file1.getName().split("\\.")[1];
+                String[] split = file1.getName().split("\\.");
+                StringBuffer sb = new StringBuffer();
+                for (int i = 1; i < split.length; i++) {
+                    System.out.println("saaa:" + split[i]);
+                    sb.append(split[i]).append(".");
+                }
+                String s1 = sb.toString().substring(0, sb.length() - 1);
                 File file01 = new File(PathKit.getWebRootPath() + "/d/" + name + "/" + name + "." + s1);
                 System.out.println("file01:" + file01.getName());
                 file1.renameTo(file01);
@@ -108,6 +118,10 @@ public class IssueTiffUtils {
                 list.add(file01.getPath());
                 if ("tiff".equals(s1) || "tif".equals(s1)) {
                     tifPath = file01.getPath();
+                }
+                if (".tif.vat.dbf".equals(s1) || ".tif.vat.dbf".equals(s1)) {
+                    existDbf = true;
+                    tifDbfPath = file01.getPath();
                 }
             }
         }
@@ -131,18 +145,126 @@ public class IssueTiffUtils {
                 Timestamp time = new Timestamp(System.currentTimeMillis());
                 String sql = "insert into ld_geoconfig (table,type,time) values('" + name + "','shp','" + time + "')";
                 Db.update(sql);
-                Kv sld = createSld(tiffFile, name);
-                Integer code = sld.getInt("code");
-                if (code == 200) {
-                    String sql1 = "update ld_geoconfig set xml = '?' where name = '?'";
-                    Db.update(sql1, sld.getStr("path"), name);
+                Kv sld;
+                if (existDbf) {
+                    sld = createSldByDbf(new File(tifDbfPath), name);
+                } else {
+                    sld = createSld(tiffFile, name);
                 }
+//                Integer code = sld.getInt("code");
+//                if (code == 200) {
+//                    String sql1 = "update ld_geoconfig set xml = '?' where name = '?'";
+//                    Db.update(sql1, sld.getStr("path"), name);
+//                }
                 return Kv.by("msg", "发布成功").set("code", 200).set("sld", sld.get("msg"));
             } else {
                 return Kv.by("msg", "发布失败").set("code", 400);
             }
         } else {
             return Kv.by("msg", "该数据已存在").set("code", 400);
+        }
+    }
+
+
+    /**
+     * 读取dbf
+     *
+     * @param tifFile
+     */
+    public static List<Double> readDBF(File tifFile) {
+        InputStream fis = null;
+        try {
+            // 读取文件的输入流
+            fis = new FileInputStream(tifFile.getAbsolutePath());
+            // 根据输入流初始化一个DBFReader实例，用来读取DBF文件信息
+            DBFReader reader = new DBFReader(fis);
+            reader.setCharactersetName("GBK");
+            // System.out.println(reader.getCharset());
+            // 调用DBFReader对实例方法得到path文件中字段的个数
+            int fieldsCount = reader.getFieldCount();
+            // 取出字段信息
+            int index = 0;
+            for (int i = 0; i < fieldsCount; i++) {
+                DBFField field = reader.getField(i);
+                String output = new String(field.getName().getBytes("iso-8859-1"), "GBK");
+                if (output.equals("value")) {
+                    index = i;
+                }
+            }
+            //配色数据
+            List<Double> doubles = new ArrayList<>();
+            Object[] rowValues; // 一条条取出path文件中记录
+            while ((rowValues = reader.nextRecord()) != null) {
+                for (int i = 0; i < rowValues.length; i++) {
+                    if (i == index) {
+                        doubles.add(((BigDecimal)rowValues[i]).doubleValue());
+                    }
+                }
+            }
+            return doubles;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                fis.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+
+    /**
+     * 根据tif的dbf生成sld
+     *
+     * @param tifFile
+     * @param storename
+     * @return
+     */
+    public static Kv createSldByDbf(File tifFile, String storename) {
+        try {
+            List<Double> doubles = readDBF(tifFile);
+            //排序
+            Collections.sort(doubles);
+            //生成sld
+            System.out.println("create new sld...");
+            Document document = DocumentHelper.createDocument();
+            Element root = document.addElement("StyledLayerDescriptor");
+            root.addAttribute("version", "1.0.0");
+            root.addAttribute("xmlns", "http://www.opengis.net/sld");
+//            root.add(new Namespace("sld", "http://www.opengis.net/sld"));
+            root.add(new Namespace("ogc", "http://www.opengis.net/ogc"));
+            root.add(new Namespace("xlink", "http://www.w3.org/1999/xlink"));
+            root.add(new Namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance"));
+            root.add(new Namespace("schemaLocation", "http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"));
+//            root.add(new Namespace("schemaLocation", "http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"));
+
+            Element row_nl = root.addElement("NamedLayer");
+            row_nl.addElement("Name").addText("d:" + storename);
+            Element row_nl_us = row_nl.addElement("UserStyle");
+            row_nl_us.addElement("Name").addText("default");
+            Element row_nl_us_f = row_nl_us.addElement("FeatureTypeStyle");
+            Element row_nl_us_r = row_nl_us_f.addElement("Rule");
+
+            Element row_nl_us_r_p = row_nl_us_r.addElement("RasterSymbolizer");
+            // <Opacity>1.0</Opacity>
+            row_nl_us_r_p.addElement("Opacity").addText("1.0");
+            Element row_nl_us_r_c_p = row_nl_us_r_p.addElement("ColorMap");
+            row_nl_us_r_c_p.addAttribute("type","values");
+            String color = "#ff2626,#ff6c2b,#ffa82b,#ffda2b,#d5ff2b,#a8ff2b,#4eff2b,#2bff9e,#2bffda,#2bf3ff,#2bd5ff,#2bb2ff,#2b8fff,#2b49ff,#762bff,#b22bff,#e42bff,#ff2be9,#ff2bb7,#ff2b76";
+            String[] colors = color.split(",");
+            for (int i = 0; i < doubles.size(); i++) {
+                row_nl_us_r_c_p.addElement("ColorMapEntry").addAttribute("color", colors[i])
+                        .addAttribute("quantity", String.valueOf(doubles.get(i))).addAttribute("label", "values");
+            }
+            //生成style文件
+            String rootPath = PathKit.getWebRootPath().replace("\\", "/");
+            String path = rootPath + "/sld/" + storename + ".sld";
+            saveDocument(document, path, "UTF-8");
+            return Kv.by("msg", "生成sld成功:" + path).set("code", "200").set("path", path);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Kv.by("msg", "生成sld失败:" + e.getMessage()).set("code", "500").set("Exception", e.getMessage());
         }
     }
 
@@ -243,10 +365,10 @@ public class IssueTiffUtils {
             String rootPath = PathKit.getWebRootPath().replace("\\", "/");
             String path = rootPath + "/sld/" + storename + ".sld";
             saveDocument(document, path, "UTF-8");
-            return Kv.by("msg", "生成sld成功:"+path).set("code", "200").set("path", path);
+            return Kv.by("msg", "生成sld成功:" + path).set("code", "200").set("path", path);
         } catch (Exception e) {
             e.printStackTrace();
-            return Kv.by("msg", "生成sld失败:"+e.getMessage()).set("code", "500").set("Exception", e.getMessage());
+            return Kv.by("msg", "生成sld失败:" + e.getMessage()).set("code", "500").set("Exception", e.getMessage());
         }
     }
 
