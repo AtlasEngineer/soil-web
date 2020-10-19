04190aeeeac7b288f2a120a27b5d3386020d1ac6
yangxueyang <1612219848@qq.com>
2020-10-19 18:17:34 +0800

    提交

--- src/main/java/com/soli/server/service/impl/TiankuaiServiceImpl.java
+++ src/main/java/com/soli/server/service/impl/TiankuaiServiceImpl.java
@@ -289,7 +289,7 @@ public class TiankuaiServiceImpl extends LambkitModelServiceImpl<Tiankuai> imple
      */
     public static boolean queryField(Record latlons, SimpleFeatureSource featureSource) throws IOException, CQLException {
         //final Filter filter = CQL.toFilter( "area < 40" );
-        Filter filter = ECQL.toFilter("INTERSECTS(the_geom,'"+ latlons.getStr("geom") + "')");
+        Filter filter = ECQL.toFilter("INTERSECTS(the_geom,"+ latlons.getStr("geom") + ")");
         SimpleFeatureCollection features = featureSource.getFeatures(filter);
         if (features.size() != 0) {
             return true;
