5ca7185e306296e759f5da6f91d74a5ecd9f8b40
wangzhengwen <wangzw9844@163.com>
2020-11-11 11:19:24 +0800

    作物上传

---
+++ src/main/java/com/soli/server/service/impl/ProductServiceImpl.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2015-2017, Henry Yang 杨勇 (gismail@foxmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soli.server.service.impl;
+
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
+import com.lambkit.common.model.LambkitModel;
+import com.lambkit.common.service.LambkitModelServiceImpl;
+import com.lambkit.core.aop.AopKit;
+import com.soli.server.model.Catalogue;
+import com.soli.server.service.CatalogueService;
+import com.soli.server.service.ProductService;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author ww
+ * @website: www.lambkit.com
+ * @email: gismail@foxmail.com
+ * @date 2020-11-11
+ * @version 1.0
+ * @since 1.0
+ */
+public class ProductServiceImpl extends LambkitModelServiceImpl implements ProductService {
+
+
+	@Override
+	public LambkitModel dao() {
+		return null;
+	}
+
+	@Override
+	public Ret getProductList(Integer pagenum, Integer pagesize, String name) {
+		String from = " FROM tr_product WHERE del = 0 ORDER BY create_time desc";
+		Page<Record> paginate = Db.paginate(pagenum, pagesize, "select id,product,phenology", from);
+		return Ret.ok("data",paginate);
+	}
+
+	@Override
+	public Ret getProductById(Integer id) {
+		Record tr_product = Db.findById("tr_product", id);
+		return Ret.ok("data",tr_product);
+	}
+
+	@Override
+	public Ret updateProduct(Integer id, String name, String phenology) {
+		Record tr_product = Db.findById("tr_product", id);
+		tr_product.set("product",name);
+		tr_product.set("phenology",phenology);
+		boolean tr_product1 = Db.update("tr_product", tr_product);
+		if (tr_product1){
+			return Ret.ok("msg","修改成功");
+		}else {
+			return Ret.fail("errorMsg","修改失败");
+		}
+	}
+
+	@Override
+	public Ret delProductByIds(String ids) {
+		String[] split = ids.split(",");
+		if (split.length > 0){
+			int update = Db.update("UPDATE tr_product SET del = 1 WHERE id in (" + ids + ")");
+		}
+		return Ret.ok("msg","删除成功");
+	}
+
+	@Override
+	public Ret getPhenologyListByName(String name) {
+		Record first = Db.findFirst("SELECT id, product, phenology FROM tr_product WHERE product = '" + name + "'");
+		String phenology = first.getStr("phenology");
+		if (StringUtils.isNotBlank(phenology)){
+			String[] split = phenology.split(",");
+			first.set("pheList",split);
+		}
+		else {
+			first.set("pheList",new String[0]);
+		}
+		return Ret.ok("msg",first);
+
+	}
+
+}
--- src/main/java/com/soli/server/utils/ExcelReaderUtils.java
+++ src/main/java/com/soli/server/utils/ExcelReaderUtils.java
@@ -2,6 +2,7 @@ package com.soli.server.utils;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
+import com.lambkit.common.util.StringUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 
@@ -57,5 +58,35 @@ public class ExcelReaderUtils {
             int update = Db.update(substring1 + ")");
         }
         return sql_ex.toString();
+    }
+
+    public static int productUpload(File file) throws IOException, InvalidFormatException {
+        Workbook workbook = WorkbookFactory.create(file);
+        //获取一张表
+        Sheet sheet = workbook.getSheetAt(0);
+        StringBuffer sql_ex = new StringBuffer();
+        sql_ex.append(" ( product,phenology,create_time,del)  VALUES ( ");
+        int suceessCount = 0;
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {//跳过第一行，取得其他行数据
+            StringBuffer sql = new StringBuffer();
+            sql.append(" INSERT INTO tr_product" + sql_ex.toString());
+            Row row = sheet.getRow(i);//取得第i行数据
+            if (StringUtils.isBlank(row.getCell(0).getStringCellValue())){
+                continue;
+            }
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                Cell cell = row.getCell(j);//取得第j列数据
+                if (cell == null) {
+                    continue;
+                }
+                String value = cell.getStringCellValue();
+                sql.append("'" + value + "',");
+            }
+            sql.append("now(),");
+            sql.append("0");
+            int update = Db.update(sql.toString() + ")");
+            suceessCount+=update;
+        }
+        return suceessCount;
     }
 }
